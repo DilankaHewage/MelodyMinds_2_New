d0c51f815409e8e53708dd0eba6cdf174739335a
DilankaHewage <dilankahewage1@gmail.com>
2025-05-21 02:24:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI); // Updated to omit deprecated options
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`Error: ${error.message}`); // Log the error message
     process.exit(1);
   }
 };
