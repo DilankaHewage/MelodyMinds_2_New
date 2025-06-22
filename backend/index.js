efe10ab312079102a575caa18bd6faeaa50d268f
DilankaHewage <dilankahewage1@gmail.com>
2025-06-22 19:20:12 +0530

    bcrypt

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(cors({
   origin: 'http://localhost:3000', // Change this if your frontend is hosted elsewhere
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+  
 }));
 
 
